a5dc0ca5fbf77e2cf335ddd3396391ef40fc58e0
Kajanan1995 <kajanan16@gmail.com>
2023-02-20 12:41:11 -0500

    Contact-us feature added

---
+++ scripts/feedback.js
@@ -0,0 +1,22 @@
+const feedback = document.getElementById("feedback");
+
+feedback.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const username = document.getElementById("username").value;
+    const city = document.getElementById("city").value;
+    const category = document.getElementById("category").value;
+    const comment = document.getElementById("comment").value;
+    console.log(comment)
+
+        
+
+        const contact = {
+            username: username,
+            city: city,
+            category: category,
+            comment: comment
+        };
+
+        localStorage.setItem("contact", JSON.stringify(contact));
+        alert("form submitted");
+    });
